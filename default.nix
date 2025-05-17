6c550be74c50e831482d6085aa8c033c723cd67b
Misael Aguayo <aguayo.misael@gmail.com>
2025-05-17 17:15:14 -0500

    Update nix build file

--- default.nix
+++ default.nix
@@ -2,16 +2,16 @@
 
 nixpkgs.rustPlatform.buildRustPackage {
     pname = "nix-search-tui";
-    version = "0.0.1";
+    version = "0.1.0";
     src = nixpkgs.fetchFromGitHub {
         owner = "misaelaguayo";
         repo = "nix-search-tui";
-        rev = "v0.0.1";
-        hash = "sha256-j3IoWr/hcPCXv5O3SSSmYi8QZ3HOYwaJTkPIAscLaFQ=";
+        rev = "v0.1.0";
+        hash = "sha256-Bnuk+28DZJV0M8Do37D58AJpEsleJf09X9x03T2dYVE=";
     };
 
     # Skip tests as they require the nix-search-cli binary, but we don't have that in pipelines
     doCheck = false;
 
-    cargoHash = "sha256-/CkguHPmTl3DdQQl6S8H6mcb+cSrfU2Osv417cf6uUw=";
+    cargoHash = "sha256-yp8+MWpyMv4ILvhuaZcHF/Q9NPmC+mwhcjXOmE9abnk=";
 }
