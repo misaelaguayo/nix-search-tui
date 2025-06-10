591b642c541ed0dd7dc4c4fc073467f832688763
Misael Aguayo <aguayo.misael@gmail.com>
2025-06-09 21:20:11 -0500

    Update nix build file to version 0.2.0

--- default.nix
+++ default.nix
@@ -6,15 +6,15 @@
 
 rustPlatform.buildRustPackage {
     pname = "nix-search-tui";
-    version = "0.1.1";
+    version = "0.2.0";
     src = fetchFromGitHub {
         owner = "misaelaguayo";
         repo = "nix-search-tui";
-        rev = "v0.1.1";
-        hash = "sha256-/B5n19FpYbbFtMVx3K7jBl6uBbesPNEf64Nxw3wvRmY";
+        rev = "v0.2.0";
+        hash = "sha256-Ksm9xZ0mFf5SVVzkHALnWCDT2aQl69IvWZgyR8dR1Mk=";
     };
 
     nativeBuildInputs = [ nix-search-cli ];
 
-    cargoHash = "sha256-IJeG30YZZNZNrlsewI/sHnQNobqvqmfcY/fP8/WAFKg=";
+    cargoHash = "sha256-IJeG30YZZNZNrlsewI/sHnQNobqvqmfcY/fP8/WAFKg";
 }
